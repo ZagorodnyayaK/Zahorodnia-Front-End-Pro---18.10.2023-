e203466499ec7864ff27983e19d1db4d64884761
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-10-26 16:17:29 +0300

    init

--- lesson3.js
+++ lesson3.js
@@ -1,9 +1,9 @@
-// average value
+// HT-7 average value
 
 const firstnumber= +prompt('Enter the first number');
 const secondnumber= +prompt('Enter the second number');
 const thirdnumber= +prompt('Enter the third number');
 const qualityOfValue = 3;
 const value = (firstnumber+secondnumber+thirdnumber)/qualityOfValue;
-alert('Average value =' +value);
+alert('Average value = ' +value);
 
