70d5903295eb4639ee06883eb463076fe7f3dd5b
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-10-26 16:15:50 +0300

    init

--- lesson3.js
+++ lesson3.js
@@ -4,4 +4,6 @@ const firstnumber= +prompt('Enter the first number');
 const secondnumber= +prompt('Enter the second number');
 const thirdnumber= +prompt('Enter the third number');
 const qualityOfValue = 3;
-alert (firstnumber+secondnumber+thirdnumber)/qualityOfValue;
+const value = (firstnumber+secondnumber+thirdnumber)/qualityOfValue;
+alert('Average value =' +value);
+
