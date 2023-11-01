75386bfc189d0a85b28e05fdc9c9c0563581993d
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-11-01 12:01:27 +0200

    init

--- Lesson4.js
+++ Lesson4.js
@@ -116,7 +116,6 @@ console.log(dividersSum)
 //12
 for (let i = 1; i<=10; i++) {
     for (let k = 1; k<=10; k++){
-        // console.log (i*k);
         console.log (`${i}*${k}=${i*k}`);
     }
 }
