5fe78a6fe5537626ed9b13f59593832e180409f0
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-11-08 17:48:41 +0200

    init

--- task-13.js
+++ task-13.js
@@ -1,15 +1,15 @@
-//
-// const userArray = prompt(`Enter an array through the coma`);
-//
-// let comma = ",";
-// let arrayOfStrings = userArray.split(comma);
-//
-// console.log(arrayOfStrings.sort());
-// arrayOfStrings.splice(2,3);
-// console.log(arrayOfStrings);
-//
-//
-//
+
+const userArray = prompt(`Enter an array through the coma`);
+
+let comma = ",";
+let arrayOfStrings = userArray.split(comma);
+
+console.log(arrayOfStrings.sort());
+arrayOfStrings.splice(2,3);
+console.log(arrayOfStrings);
+
+
+
 
 
 
--- task-14.js
+++ task-14.js
@@ -1,33 +1,28 @@
 
-// const arrayNew = [1, 2, 3, 4, 5, 6, 7];
-//
-// function removeElement(array, item) {
-//     let foundNum = array.indexOf(item);
-//     array.splice(foundNum,1);
-//
-//     return array;
-// }
-// removeElement(arrayNew,5);
-// console.log(arrayNew);
+const arrayNew = [1, 2, 3, 4, 5, 6, 7];
+
+const removeElement = (array, item) =>{
+    let foundNum = array.indexOf(item);
+    array.splice(foundNum,1);
+
+    return array;
+}
+removeElement(arrayNew,5);
+console.log(arrayNew);
 
 
-function removeElement2(array, item){
+
+const removeElement2 = (array, item) =>{
     let index = 0;
     let arr = [];
     for (let i = 0; i < array.length; i++){
-        if (array[i] === item ) {
-            arr[index] = array;
+        if (array[i] !== item ) {
+            arr[index] = array[i];
             index++;
-            arr = [];
-            console.log(arr)
-        } else {
-            array[i] !== item;
-
         }
     }
     return arr;
 }
-arrNew = str?str.split(','):[];
 
-console.log(removeElement2(arrNew,2));
+console.log(removeElement2(arrayNew,5));
 
