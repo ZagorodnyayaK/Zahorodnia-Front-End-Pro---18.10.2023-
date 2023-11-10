21623f1231d492caaee7488e675fc1b561e93651
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-11-10 12:32:56 +0200

    init

--- task-17.js
+++ task-17.js
@@ -1,51 +1,45 @@
 //task-1
-// const arrayRandom = [123, `Hi`, 56.7, -0.1, 90, `key`, `num`, 404, `qwerty`, 2099, -5, 2023, 1.1];
-// const filterOfNumber = (arrayRandom.filter(itemNum => typeof itemNum === `number`));
-// const averageValue = (arrayOfNumber) => {
-//     return arrayOfNumber.reduce((a, b) => (a + b)) / arrayOfNumber.length;
-// }
-//
-// console.log(Math.round((averageValue(filterOfNumber))));
-//
-//
-// const averageValue2 = (arrayOfNumber) => {
-//     let sum = 0;
-//     for (let i = 0; i < arrayOfNumber.length; i++) {
-//         sum += arrayOfNumber[i];
-//     }
-//     return sum / arrayOfNumber.length;
-// }
-// console.log(averageValue2(filterOfNumber));
-
-//task-2
-// const doMath = (x, znak, y) => {
-//     switch (znak) {
-//         case `+`:
-//             return x + y;
-//         case `-`:
-//             return x - y;
-//         case `*`:
-//             return x * y;
-//         case `/`:
-//             return x / y;
-//         case `%`:
-//             return x % y;
-//         case `^`:
-//             return x ** y;
-//     }
-// }
-//
-// console.log('The result of a mathematical operation = ' + doMath(9, `^`, 6));
-
-
-var arr = [];
-2
-for ( var i = 0; i < 20; i++ ) {
-    3
-    arr.push(Math.round(Math.random() * 100));
+const arrayRandom = [123, `Hi`, 56.7, -0.1, 90, `key`, `num`, 404, `qwerty`, 2099, -5, 2023, 1.1];
+const filterOfNumber = (arrayRandom.filter(itemNum => typeof itemNum === `number`));
+const averageValue = (arrayOfNumber) => {
+    return arrayOfNumber.reduce((a, b) => (a + b)) / arrayOfNumber.length;
 }
 
+console.log(Math.round((averageValue(filterOfNumber))));
 
+
+const averageValue2 = (arrayOfNumber) => {
+    let sum = 0;
+    for (let i = 0; i < arrayOfNumber.length; i++) {
+        sum += arrayOfNumber[i];
+    }
+    return sum / arrayOfNumber.length;
+}
+console.log(averageValue2(filterOfNumber));
+
+// task-2
+const doMath = (x, znak, y) => {
+    switch (znak) {
+        case `+`:
+            return x + y;
+        case `-`:
+            return x - y;
+        case `*`:
+            return x * y;
+        case `/`:
+            return x / y;
+        case `%`:
+            return x % y;
+        case `^`:
+            return x ** y;
+    }
+}
+
+console.log('The result of a mathematical operation = ' + doMath(9, `^`, 6));
+
+
+
+//task-3
 const twoDArray3 = (item1, item2, arr) => {
     let array = new Array(item1);
     for (let i = 0; i < item1; i++) {
@@ -57,21 +51,19 @@ const twoDArray3 = (item1, item2, arr) => {
     return array
 }
 
-console.log(twoDArray3(3,4,5))
+console.log(twoDArray3(5,5,5))
 
 
 
+// task-4
+const func = (string, charToDell) => {
+    charToDell.forEach((element) => {
+        string = string.toLowerCase().replaceAll(element.toLowerCase(), ``);
+    })
+    return string.trim()
+}
 
+console.log(func(`Hello world`, [`l`, `d`]));
 
-// // task-4
-// const func = (string, charToDell) => {
-//     charToDell.forEach((element) => {
-//         string = string.toLowerCase().replaceAll(element.toLowerCase(), ``);
-//     })
-//     return string.trim()
-// }
-//
-// console.log(func(`Hello world`, [`o`, `d`]));
-//
 
 
