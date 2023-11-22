cf0b66b61ff4fc7e4e2ddd1d8f18400c438109d3
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-11-22 20:25:03 +0200

    init

---
+++ task-24.js
@@ -0,0 +1,49 @@
+class Hamburger {
+    // static SIZE_SMALL = {
+    //     calories: 20,
+    //     price: 50,
+    // }
+    // static SIZE_BIG = {
+    //     calories: 40,
+    //     price: 100,
+    // }
+    // static STUFFING_CHEESE = {
+    //     calories: 20,
+    //     price: 10,
+    // }
+    // static STUFFING_SALAD = {
+    //     calories: 5,
+    //     price: 20,
+    // }
+    // static SIZE_POTATOES = {
+    //     calories: 10,
+    //     price: 15,
+    // }
+    //
+    // static TOPPING_MAYO = {
+    //     calories: 5,
+    //     price: 20,
+    // }
+    // static TOPPING_SAUCE = {
+    //     calories: 0,
+    //     price: 15,
+    // }
+static BIG_SIZE = `L`;
+static SMALL_SIZE = `S`;
+    static STUFFING_CHEESE = `cheese`;
+    static STUFFING_SALAD = `salad`;
+
+
+    addTopping() {
+
+    }
+
+    calculatePrice() {
+
+    }
+
+    calculateCalories() {
+
+    }
+
+}
