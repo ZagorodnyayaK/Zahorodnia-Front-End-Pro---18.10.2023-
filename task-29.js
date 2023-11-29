ffeb446555ef1f2202797cba8e4cea74e5225127
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-11-29 19:34:47 +0200

    init

--- task-29.js
+++ task-29.js
@@ -9,16 +9,16 @@ class User {
 }
 
 const arrayOfUsers = [
-    new User(1,"Igor",25),
-    new User(2,"Vova",44),
-    new User(3,"Ira",28),
-    new User(4,"Inna",22),
-    new User(5,"Nina",45)
+    new User(1, "Igor", 25),
+    new User(2, "Vova", 44),
+    new User(3, "Ira", 28),
+    new User(4, "Inna", 22),
+    new User(5, "Nina", 45)
 ];
 
 const myArrayUser = arrayOfUsers.reduce(
     (acc, object) => {
-        const id = object.age;
+        const id = object.id;
         acc[id] = object;
         return acc;
     },
