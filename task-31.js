52dfd65b9c5f195c7fb5d0c4219820d862718db3
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-12-11 15:22:14 +0200

    init

--- task-31.js
+++ task-31.js
@@ -1,2 +1,13 @@
 `use strict`;
 
+const divGhost = document.querySelector(`.ghost`);
+const input = document.querySelector(`.my_input`);
+
+input.addEventListener(`focus`, () => {
+    divGhost.style.visibility = "visible";
+});
+input.addEventListener(`blur`, () => {
+    divGhost.style.visibility = "hidden";
+});
+
+
