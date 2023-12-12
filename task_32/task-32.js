a5631d051177c140f896f14f932a52cb93eab1a2
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-12-12 15:26:52 +0200

    init

--- task_32/task-32.js
+++ task_32/task-32.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const btn = document.querySelector(`.changeImg`);
-const arrImages = [`1.jpg`,`2.jpg`,`3.jpg`,`4.jpg`,`5.jpg`,`6.jpg`,`7.jpg`,`8.jpg`,`9.jpg`];
+const arrImages = [`1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`, `5.jpg`, `6.jpg`, `7.jpg`, `8.jpg`, `9.jpg`];
 
 
-
- function getRandomImg(arrImages){
+function getRandomImg(arrImages) {
     let randIndex = Math.floor(Math.random() * (arrImages.length + 0 - 0));
     return `images/${arrImages[randIndex]}`;
 }
-function initRandImg(arrImages){
+
+function initRandImg(arrImages) {
     const img = document.getElementById(`rand_img`);
     img.src = getRandomImg(arrImages);
 }
 
-btn.addEventListener(`click`,() => initRandImg(arrImages));
+btn.addEventListener(`click`, () => initRandImg(arrImages));
