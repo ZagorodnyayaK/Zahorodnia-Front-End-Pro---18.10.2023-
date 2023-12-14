824b4d686bb4de4183d9cf55d78467f6561661eb
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-12-14 16:35:19 +0200

    init

--- task-34.js
+++ task-34.js
@@ -17,13 +17,4 @@ followLink.addEventListener('click', () => {
 
 
 
-const divGhost = document.querySelector(`.ghost`);
-const input = document.querySelector(`.my_input`);
-
-input.addEventListener(`focus`, () => {
-    divGhost.style.display = "inline";
-});
-input.addEventListener(`blur`, () => {
-    divGhost.style.display = "none";
-});
 
