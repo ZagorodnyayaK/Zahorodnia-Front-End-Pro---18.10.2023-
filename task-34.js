593e55c87c9e7c167de4ebbecdefee547ee36282
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-12-14 16:27:20 +0200

    init

--- task-34.js
+++ task-34.js
@@ -14,3 +14,16 @@ followLink.addEventListener('click', () => {
         window.location.href = link;
     }
 });
+
+
+
+const divGhost = document.querySelector(`.ghost`);
+const input = document.querySelector(`.my_input`);
+
+input.addEventListener(`focus`, () => {
+    divGhost.style.display = "inline";
+});
+input.addEventListener(`blur`, () => {
+    divGhost.style.display = "none";
+});
+
