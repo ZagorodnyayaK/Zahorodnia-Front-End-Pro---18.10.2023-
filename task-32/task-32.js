539887b6349024994a7b9bb1d6780dd90bbc7ef8
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-12-15 15:33:47 +0200

    init

--- task-32/task-32.js
+++ task-32/task-32.js
@@ -1,24 +1,38 @@
 'use strict';
 
-const length = 10;
 const tbody = document.querySelector('table > tbody');
-const refreshButton = document.querySelector('.refresh')
+const refreshButton = document.querySelector('.fill-table');
 
+const addTable = () => {
+    if (!tbody.childNodes) {
+        genTable();
+    } else {
+        {
+            while (tbody.firstChild) {
+                tbody.firstChild.remove()
+            }
+            genTable();
+        }
+    }
+}
 
-    Array.from({length}, () => {
+const genTable = () => {
+    for (let i = 0; i <= 10; i++) {
         const tr = document.createElement('tr');
         tr.classList.add('line');
-        Array.from({length}, () => {
+        for (let i = 0; i <= 10; i++) {
             const n = 1 + Math.random() * 100 | 0;
             const td = document.createElement('td');
             td.classList.add('box');
             td.innerText = n;
             tr.appendChild(td);
-        });
+        }
         tbody.appendChild(tr);
-    })
+    }
+};
 
-
-refreshButton.addEventListener('click', () =>{
-    window.location.reload()
+refreshButton.addEventListener('click', () => {
+    addTable()
 });
+
+
