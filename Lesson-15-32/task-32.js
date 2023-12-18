bcc46d675496918c82b35159b924e6cf6fe45d78
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-12-18 16:38:46 +0200

    init

--- Lesson-15-32/task-32.js
+++ Lesson-15-32/task-32.js
@@ -2,11 +2,14 @@
 
 const tbody = document.querySelector('table > tbody');
 const refreshButton = document.querySelector('.fill-table');
+const divTable = document.querySelector('.table')
+
 
 const addTable = () => {
     if (!tbody.childNodes) {
         genTable();
     } else {
+        refreshButton.innerHTML = 'Refresh numbers';
         {
             while (tbody.firstChild) {
                 tbody.firstChild.remove()
@@ -33,5 +36,6 @@ const genTable = () => {
 
 
 refreshButton.addEventListener('click', () => {
+    divTable.style.display = 'table-cell';
     addTable()
-});
+})
