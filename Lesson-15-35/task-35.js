aba663e4c4b2b72202964cbad8f660138c7f747f
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-12-18 21:15:13 +0200

    init

--- Lesson-15-35/task-35.js
+++ Lesson-15-35/task-35.js
@@ -1,11 +1,10 @@
 'use string';
 
 const smile = document.querySelectorAll('.smile');
-const count = document.querySelectorAll('.count-vote');
 
 smile.forEach((element) => {
     element.addEventListener('click', (e) => {
         let countEl = e.target.parentNode.querySelector('.count-vote');
         ++countEl.textContent
     })
-})
+});
