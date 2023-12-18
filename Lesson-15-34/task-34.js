cac50081d39f13ca0274010df89782c1838611e2
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-12-18 17:23:42 +0200

    init

---
+++ Lesson-15-34/task-34.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const inputSite = document.querySelector('.input-link');
+const followLink = document.querySelector('.validation-button');
+
+followLink.addEventListener('click', () => {
+    let link = inputSite.value;
+    if (!link) {
+        alert('Link is missing');
+    } else {
+        if (link.startsWith('http') === false && link.startsWith('https') === false) {
+            link = `https://${link}`;
+        }
+        window.location.href = link;
+    }
+});
