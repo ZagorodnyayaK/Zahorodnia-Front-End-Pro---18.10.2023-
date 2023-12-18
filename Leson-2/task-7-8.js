ae00343988e50b61882b248992c4c805cd667d0c
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2023-12-18 16:46:10 +0200

    init

---
+++ Leson-2/task-7-8.js
@@ -0,0 +1,43 @@
+// Hello user
+const name= prompt('Enter your name','');
+alert('Hello, '+name+', How are you?');
+
+// Numbers and rows
+const name1 = prompt('Enter your name');
+const surname = prompt('Enter your surname');
+const city = prompt('What city do you live in?');
+alert(name1+' '+surname+' '+'from'+' '+city);
+
+const numbers= prompt('Еnter five numbers','xxxxx');
+alert (numbers[0]+(" ")+numbers[1]+(" ")+numbers[2]+(" ")+numbers[3]+(" ")+numbers[4]+(" "));
+
+// Mathematical operations
+const operNumb1  = +prompt("Enter first operand");
+const operNumb2 = +prompt("Enter second operand");
+const result1 = operNumb1+operNumb2;
+const result2 = operNumb1-operNumb2;
+const result3 = operNumb1*operNumb2;
+const result4 = operNumb1/operNumb2;
+
+alert(operNumb1+'+'+operNumb2+'='+result1);
+alert(operNumb1+'-'+operNumb2+'='+result2);
+alert(operNumb1+'*'+operNumb2+'='+result3);
+alert(operNumb1+'/'+operNumb2+'='+result4);
+
+// Calculator
+const operations= prompt('Enter the operation to be calculated','add or sub or mult or div');
+const operand1  = +prompt("Enter first operand");
+const operand2  = +prompt("Enter second operand");
+const res1 = operand1+operand2;
+const res2 = operand1-operand2;
+const res3 = operand1*operand2;
+const res4 = operand1/operand2;
+alert(operand1+'+'+operand2+'='+res1);
+alert(operand1+'-'+operand2+'='+res2);
+alert(operand1+'*'+operand2+'='+res3);
+alert(operand1+'/'+operand2+'='+res4);
+
+// Hours
+const allTime  = +prompt("Enter the number of hours");
+const hours = allTime*60*60;
+alert(hours+'sec.');
