417ac2e6676760db863f68eb46cb6f73cc66eec4
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2024-01-03 20:46:47 +0200

    init

--- Lesson-16-37/task-37.js
+++ Lesson-16-37/task-37.js
@@ -1,2 +1,22 @@
 'use strict';
 
+const imagesArray = ['nature.jpg', 'sunset.jpg', 'tree.jpg', 'trees.jpg'];
+
+
+document.querySelector('.prev').addEventListener('click', (ev) => {
+    const current = ev.target.src;
+    const index = imagesArray.indexOf(current);
+    ev.target.src = imagesArray[index - 1];
+});
+document.querySelector('.next').addEventListener('click', (ev) => {
+    let current = ev.target.parentNode.querySelector('img').src;
+    let imgSplited = current.split('/');
+    current = imgSplited[imgSplited.length -1];
+    console.log(ev.target,current);
+    const index = imagesArray.indexOf(current);
+    console.log(index);
+    ev.target.src = imagesArray[index + 1];
+});
+
+
+
