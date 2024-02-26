9c365066cb72c826af9869b10b6fb59fd5dd5434
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2024-02-26 17:19:26 +0200

    init

--- Lesson-16-37/task-37.js
+++ Lesson-16-37/task-37.js
@@ -1,22 +1,30 @@
 'use strict';
 
-const imagesArray = ['nature.jpg', 'sunset.jpg', 'tree.jpg', 'trees.jpg'];
+const slider = document.querySelectorAll(".slider img");
+let slideIndex = 0;
+let intervalId = null;
 
+document.addEventListener("DOMContentLoaded", initializeSlider);
 
-document.querySelector('.prev').addEventListener('click', (ev) => {
-    const current = ev.target.src;
-    const index = imagesArray.indexOf(current);
-    ev.target.src = imagesArray[index - 1];
-});
-document.querySelector('.next').addEventListener('click', (ev) => {
-    let current = ev.target.parentNode.querySelector('img').src;
-    let imgSplited = current.split('/');
-    current = imgSplited[imgSplited.length -1];
-    console.log(ev.target,current);
-    const index = imagesArray.indexOf(current);
-    console.log(index);
-    ev.target.src = imagesArray[index + 1];
-});
+const initializeSlider = () => {
+    if (slider.length > 0) {
+        slider[slideIndex].classList.add("displaySlide");
+        intervalId = setInterval(nextSlide, 5000);
+    }
+}
 
+const showSlide = (index) => {
+    slider.forEach(slide =>{
+        slide.classList.remove("displaySlide");
+    });
+    slider[slideIndex].classList.add("displaySlide");
+}
 
+const prevSlide = () => {
+
+}
+const nextSlide = () => {
+    slideIndex++;
+    showSlide(slideIndex);
+}
 
--- fe-pro-01-template/src/app.js
+++
@@ -1,7 +0,0 @@
-import initMenu from './components/menu/menu.js';
-import initProductsList from './components/products-list/products-list.js';
-
-document.addEventListener('DOMContentLoaded', async () => {
-  initMenu();
-  initProductsList();
-});
