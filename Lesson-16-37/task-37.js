c9da76fff19afeec5e4581ba5c1331818b6a80c6
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2024-02-26 17:48:17 +0200

    init

--- Lesson-16-37/task-37.js
+++ Lesson-16-37/task-37.js
@@ -1,29 +1,40 @@
 'use strict';
 
 const slider = document.querySelectorAll(".slider img");
+const sliderPrev = document.querySelector(".slider-prev")
+const sliderNext = document.querySelector(".slider-next")
 let slideIndex = 0;
 let intervalId = null;
 
 document.addEventListener("DOMContentLoaded", initializeSlider);
-
-const initializeSlider = () => {
+sliderPrev.addEventListener('click', prevSlide);
+sliderNext.addEventListener('click', nextSlide);
+function initializeSlider() {
     if (slider.length > 0) {
         slider[slideIndex].classList.add("displaySlide");
         intervalId = setInterval(nextSlide, 5000);
     }
 }
 
-const showSlide = (index) => {
-    slider.forEach(slide =>{
+function showSlide(index) {
+    if (index >= slider.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slider.length - 1;
+    }
+
+    slider.forEach(slide => {
         slide.classList.remove("displaySlide");
     });
     slider[slideIndex].classList.add("displaySlide");
 }
 
-const prevSlide = () => {
-
+function prevSlide ()  {
+    clearInterval(intervalId);
+    slideIndex--;
+    showSlide(slideIndex);
 }
-const nextSlide = () => {
+function nextSlide ()  {
     slideIndex++;
     showSlide(slideIndex);
 }
